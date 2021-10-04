fcab24376e1006c68fc1a9b6d7f0e5bec5d29921
joel.lopez1 <joel.lopez@uab.edu.bo>
2021-10-03 23:29:32 -0400

    tarea 1 curso de php

---
+++ tarea1.php
@@ -0,0 +1,11 @@
+<?php
+echo "TAREA 1 <br>";
+echo "ESTUDIANTE: JOEL JHOVANNI LOPEZ PAREDES <br>";
+#Declaramos las variables
+$variable1=3;
+$variable2=2;
+#sumamos las variables
+$resultado= $variable1 + $variable2;
+#imprimimos el resultado
+echo "El resultado de la suma de ".$variable1." + ".$variable2." es ".$resultado;
+?>
